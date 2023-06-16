8bd0aa909ccb81b8e2c8d230c8e792a7fb191867
fatemeh-mgsdi <fatemeh.maghsoudi76@gmail.com>
2023-06-16 18:16:12 +0330

    Add delete to `IsSuperUser` permission

--- cryptocurrency/permissions.py
+++ cryptocurrency/permissions.py
@@ -3,6 +3,6 @@ from rest_framework import permissions
 
 class IsSuperUser(permissions.BasePermission):
     def has_permission(self, request, view):
-        if request.method in ['POST', 'PATCH', 'PUT']:
+        if request.method in ["POST", "PATCH", "PUT", "DELETE"]:
             return request.user and request.user.is_superuser
         return True
