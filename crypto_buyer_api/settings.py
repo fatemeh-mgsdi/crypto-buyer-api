2f8503fc3419a4d022073dbc4a379f865a5e9502
fatemeh-mgsdi <fatemeh.maghsoudi76@gmail.com>
2023-06-16 15:55:11 +0330

    Add swagger

--- crypto_buyer_api/settings.py
+++ crypto_buyer_api/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     "cryptocurrency",
     "deposit",
     "wallet",
+    "drf_yasg",
 ]
 
 MIDDLEWARE = [
--- crypto_buyer_api/urls.py
+++ crypto_buyer_api/urls.py
@@ -16,6 +16,23 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.urls import include, path, re_path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
+from rest_framework.authentication import TokenAuthentication
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Crypto Buyer",
+        default_version="v1.0.0",
+        description="Crypto buyer API",
+    ),
+    url="http://127.0.0.1:8000/",
+    public=True,
+    permission_classes=[permissions.AllowAny],
+    authentication_classes=[TokenAuthentication],
+)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -23,4 +40,14 @@ urlpatterns = [
     path("api/", include("cryptocurrency.urls")),
     path("api/", include("deposit.urls")),
     path("api/", include("wallet.urls")),
+    re_path(
+        r"^swagger(?P<format>\.json|\.yaml)$",
+        schema_view.without_ui(cache_timeout=0),
+        name="schema-json",
+    ),
+    re_path(
+        r"^swagger/$",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
 ]
