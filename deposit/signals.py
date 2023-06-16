8b49fb9ff9e20c03aa6191a2d78789f941f1b62a
fatemeh-mgsdi <fatemeh.maghsoudi76@gmail.com>
2023-06-16 13:47:07 +0330

    Pass crypto `symbol` instead of `name`

--- deposit/signals.py
+++ deposit/signals.py
@@ -8,9 +8,9 @@ from deposit.exchange_handler import buy_from_exchange
 
 @receiver(post_save, sender=PendingTransaction)
 def check_pending_transactions(sender, instance, created, **kwargs):
-    total_amounts = PendingTransaction.objects.select_related('crypto').values('crypto__name').annotate(total=Sum('amount'))
+    total_amounts = PendingTransaction.objects.select_related('crypto').values('crypto__symbol').annotate(total=Sum('amount'))
 
     for total_amount in total_amounts:
         if total_amount['total'] >= 10:
-            buy_from_exchange(total_amount['crypto__name'], total_amount['total'])
-            PendingTransaction.objects.filter(crypto__name=total_amount['crypto__name']).delete()
+            buy_from_exchange(total_amount['crypto__symbol'], total_amount['total'])
+            PendingTransaction.objects.filter(crypto__symbol=total_amount['crypto__symbol']).delete()
